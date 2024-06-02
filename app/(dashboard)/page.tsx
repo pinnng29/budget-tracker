9f336d370a94bddff21857e0b70dce597238c76d
Aping <apingspeedholic@gmail.com>
2024-06-03 01:12:57 +0700

    Overview Section

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
 import WelcomeMsg from "./_components/welcome-msg";
+import Overview from "./_components/overview";
 
 export default function Dashboard() {
   return (
     <div className="h-full bg-background">
       <WelcomeMsg />
+      <Overview />
     </div>
   )
 }
